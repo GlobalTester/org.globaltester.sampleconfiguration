c7bc51d038dbee383dae9dfabb350bdc5a0abb45
Alexander May <alexander.may@hjp-consulting.com>
2012-02-22 14:10:03 +0000

    first test for CardConfigObject
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration.test/trunk@668 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

---
+++ src/org/globaltester/cardconfiguration/CardConfigTest.java
@@ -0,0 +1,16 @@
+package org.globaltester.cardconfiguration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardConfigTest {
+	
+	private final String DEFAULT_MRZ = "P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<C11T002JM4D<<9608122F1310317<<<<<<<<<<<<<<<6";
+
+	@Test
+	public void testGetMrz() {
+		assertEquals(DEFAULT_MRZ, new CardConfig().get("ICAO9303", "MRZ"));
+	}
+
+}
