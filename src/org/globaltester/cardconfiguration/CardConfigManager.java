98d64216d13a79c5039ee8f27de6939114fc94f9
Alexander May <alexander.may@hjp-consulting.com>
2012-03-08 09:28:27 +0000

    add method getAvailableConfigNames() and adapt implementation to a more generic version
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration/trunk@777 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/cardconfiguration/CardConfigManager.java
+++ src/org/globaltester/cardconfiguration/CardConfigManager.java
@@ -1,13 +1,21 @@
 package org.globaltester.cardconfiguration;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class CardConfigManager {
 
+	private static HashMap<String, CardConfig> configs = new HashMap<String, CardConfig>();
+	
 	//FIXME AMY replace the following stubs with a logic that stores/retrieves CardConfigs from workspace
 	public static String DEFAULT_CARD_CONFIG = "nPA mustermann Erika";
-	private static CardConfig mustermannErika = new CardConfig(DEFAULT_CARD_CONFIG);
 	static {
+		CardConfig mustermannErika = createNewCardConfig(DEFAULT_CARD_CONFIG);
 		mustermannErika.put("ICAO9303", "MRZ", "P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<C11T002JM4D<<9608122F1310317<<<<<<<<<<<<<<<6");
+
 	}
+	
+	
 	
 	/**
 	 * Return the card config with the given name. creates it if it does not
@@ -17,7 +25,21 @@ public class CardConfigManager {
 	 * @return
 	 */
 	public static CardConfig get(String cardConfigName) {
-		return mustermannErika;
+		if (configs.containsKey(cardConfigName)) {
+			return configs.get(cardConfigName);
+		} else {
+			return createNewCardConfig(cardConfigName);
+		}
+	}
+
+	private static CardConfig createNewCardConfig(String cardConfigName) {
+		CardConfig newConfig = new CardConfig(cardConfigName);
+		configs.put(cardConfigName, newConfig);
+		return newConfig;
+	}
+
+	public static Set<String> getAvailableConfigNames() {
+		return configs.keySet();
 	}
 
 }
