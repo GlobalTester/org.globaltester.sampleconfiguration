a06071291ea576101c6660d09e7837e5d9c6070e
Alexander May <alexander.may@hjp-consulting.com>
2012-03-13 08:03:43 +0000

    created Nature and Project for CardConfigs
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration/trunk@803 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/cardconfiguration/Activator.java
+++ src/org/globaltester/cardconfiguration/Activator.java
@@ -5,6 +5,8 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+	public static final String PLUGIN_ID = "org.globaltester.cardconfiguration";
+	
 	private static BundleContext context;
 
 	static BundleContext getContext() {
---
+++ src/org/globaltester/cardconfiguration/GtCardConfigNature.java
@@ -0,0 +1,32 @@
+package org.globaltester.cardconfiguration;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class GtCardConfigNature implements IProjectNature {
+	
+	public static final String NATURE_ID = "org.globaltester.cardconfiguration.gtCardConfigNature"; //$NON-NLS-1$
+	private IProject project = null;
+
+	@Override
+	public void configure() throws CoreException {
+		// currently no action needed here
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+		// currently no action needed here		
+	}
+
+	@Override
+	public IProject getProject() {
+		return this.project;
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		this.project = project;
+	}
+
+}
