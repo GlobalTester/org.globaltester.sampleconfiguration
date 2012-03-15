1e1cc0788e9959adc8f7b56338501642301cc846
Alexander May <alexander.may@hjp-consulting.com>
2012-03-15 09:49:20 +0000

    make card configs available imediatealy after import of project
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration/trunk@824 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/cardconfiguration/CardConfigManager.java
+++ src/org/globaltester/cardconfiguration/CardConfigManager.java
@@ -21,7 +21,7 @@ public class CardConfigManager {
 	static {
 		for (String curConfigName : getAvailableConfigNames()) {
 			IProject curProject = ResourcesPlugin.getWorkspace().getRoot()
-			.getProject(curConfigName);
+					.getProject(curConfigName);
 			CardConfig curConfig = new CardConfig(curProject);
 			configs.put(curConfigName, curConfig);
 		}
@@ -30,22 +30,28 @@ public class CardConfigManager {
 	private static String DEFAULT_CARD_CONFIG = "Mustermann Erika";
 
 	/**
-	 * Return the card config with the given name. 
-	 * Returns DEFAULT_CARD_CONFIG if not present.
-	 * exist yet
+	 * Return the card config with the given name. Returns DEFAULT_CARD_CONFIG
+	 * if not present. exist yet
 	 * 
 	 * @param cardConfigName
 	 * @return
 	 */
 	public static CardConfig get(String cardConfigName) {
 		if (!configs.containsKey(cardConfigName)) {
-			return getDefaultConfig();
+			if (getAvailableConfigNames().contains(cardConfigName)) {
+				CardConfig newConfig = new CardConfig(ResourcesPlugin
+						.getWorkspace().getRoot().getProject(cardConfigName));
+				configs.put(cardConfigName, newConfig);
+			} else {
+				return getDefaultConfig();
+			}
 		}
 		return configs.get(cardConfigName);
 	}
 
 	private static CardConfig createNewCardConfig(String cardConfigName) {
-		// FIXME AMY CardConfig remove the method CreateNewCardConfig and migrate it to a Wizard
+		// FIXME AMY CardConfig remove the method CreateNewCardConfig and
+		// migrate it to a Wizard
 		IProject project = GtCardConfigProject.createProject(cardConfigName,
 				null);
 		CardConfig newConfig = new CardConfig(project);
@@ -65,7 +71,7 @@ public class CardConfigManager {
 					.put("ICAO9303",
 							"MRZ",
 							"P<D<<MUSTERMANN<<ERIKA<<<<<<<<<<<<<<<<<<<<<<C11T002JM4D<<9608122F1310317<<<<<<<<<<<<<<<6");
-			
+
 			try {
 				newConfig.saveToProject();
 			} catch (CoreException e) {
