f5eb984f8852f2f2965e527571d78223a1b4e13a
Alexander May <alexander.may@hjp-consulting.com>
2012-05-16 19:37:05 +0000

    correctly wrap MRZ
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration.ui/trunk@979 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/cardconfiguration/ui/CardConfigEditorWidget.java
+++ src/org/globaltester/cardconfiguration/ui/CardConfigEditorWidget.java
@@ -77,10 +77,31 @@ public class CardConfigEditorWidget {
 	private void updateTabItemProtocols() {
 		String mrzString = (String) getCardConfig().get("ICAO9303", "MRZ");
 		if (mrzString != null) {
-			// use methods from MRZ class after refactoring to protocol
-			mrz1.setText(mrzString.substring(0, 44));
-			mrz2.setText(mrzString.substring(44));
-			mrz3.setText("");
+
+			// TODO use methods from MRZ class after refactoring to protocol
+			switch (mrzString.length()) {
+			case 90: //ID-1 / TD-1
+				mrz1.setText(mrzString.substring(0, 30));
+				mrz2.setText(mrzString.substring(31, 60));
+				mrz3.setText(mrzString.substring(60));
+				break;
+			case 72: //ID-2 / TD-2
+				mrz1.setText(mrzString.substring(0, 36));
+				mrz2.setText(mrzString.substring(36));
+				mrz3.setText("");
+				break;
+			case 88: //ID-3 / TD-3
+				mrz1.setText(mrzString.substring(0, 44));
+				mrz2.setText(mrzString.substring(44));
+				mrz3.setText("");
+				break;
+
+			default: //unkown format
+				mrz1.setText(mrzString);
+				mrz2.setText("");
+				mrz3.setText("");
+				break;
+			}
 		} else {
 			mrz1.setText("");
 			mrz2.setText("");
