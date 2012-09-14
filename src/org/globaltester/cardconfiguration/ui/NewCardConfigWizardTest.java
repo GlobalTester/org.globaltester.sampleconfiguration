8865bc2d66e83beb3c214e5157b3c90e840a733b
Martin Boonk <martin.boonk@hjp-consulting.com>
2012-09-14 10:02:42 +0000

    using uihelpers in cardconfigwizard test
    git-svn-id: http://subversion.globaltester.org/repo/globaltester/org.globaltester.cardconfiguration.ui.test/trunk@1204 cd90bb66-5c64-11e0-9e8e-9b0b1919da2d

--- src/org/globaltester/cardconfiguration/ui/NewCardConfigWizardTest.java
+++ src/org/globaltester/cardconfiguration/ui/NewCardConfigWizardTest.java
@@ -1,70 +1,39 @@
 package org.globaltester.cardconfiguration.ui;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
-import org.eclipse.swtbot.swt.finder.waits.Conditions;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.globaltester.cardconfiguration.GtCardConfigNature;
-import org.globaltester.rcp.ApplicationWorkbenchWindowAdvisor;
+import org.globaltester.swtbot.Strings;
+import org.globaltester.swtbot.uihelper.CardConfigWizardUiHelper;
+import org.globaltester.swtbot.uihelper.GlobalTesterUiHelper;
 import org.junit.Before;
 import org.junit.Test;
 
 public class NewCardConfigWizardTest {
-
-	private SWTWorkbenchBot bot = new SWTWorkbenchBot();
-
+	
 	@Before
-	public void setUpClass() {
-		bot.waitUntil(Conditions
-				.shellIsActive(ApplicationWorkbenchWindowAdvisor.gtMainWindowTitle));
-
-		SWTBotView activeView = bot.activeView();
-
-		// close the welcome page, if it is shown
-		if ("Welcome".equals(activeView.getTitle())) {
-			bot.viewByTitle("Welcome").close();
-		}
-	}
-
-	public void openNewCardConfigWizard() {
-		bot.menu("File").menu("New").click();
-
-		SWTBotShell newDialogBot = bot.shell("New");
-		SWTBotTree newDialogTree = newDialogBot.bot().tree();
-
-		newDialogTree.expandNode("GlobalTester", true).select("CardConfiguration");
-
-		bot.button("Next >").click();
-
+	public void setUpClass() throws CoreException {
+		GlobalTesterUiHelper.init();
 	}
 
 	@Test
 	public void testCreateNewDefaultCardConfig() throws CoreException {
 		String cardConfigName = "MustermannErika";
-
-		openNewCardConfigWizard();
-
-		bot.captureScreenshot("screenshots" + File.separator
-				+ "newCardConfigWizard.png");
-
-		bot.text().setText(cardConfigName);
-		bot.button("Next >").click();
 		
-		bot.button("Finish").click();
+		CardConfigWizardUiHelper wizard = GlobalTesterUiHelper.openNewWizardByMenu().selectCardConfiguration();
+		wizard.setProjectName(cardConfigName);
+		wizard.captureScreenshot(Strings.FILE_SCREENSHOTS_SUBFOLDER + File.separator + "newCardConfigWizard.png");
+		wizard.finish();
 		
 		IProject createdProject = ResourcesPlugin.getWorkspace().getRoot()
 		.getProject(cardConfigName);
 		
 		assertTrue("Project was not created", createdProject.exists());
 		assertTrue("Project does not contain nature", createdProject.hasNature(GtCardConfigNature.NATURE_ID));
-
 	}
 }
